5770082279f3d1e7faf13e48605a2c762e0f14f1
Kavindu Athaudha <kavindu@kih.com.sg>
2025-02-10 01:33:11 +1300

    Fixed the wrong GitHub links.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lieutenant-ecosystem/lieutenant-ecosystem.github.io',
         },
         blog: {
           showReadingTime: true,
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lieutenant-ecosystem/lieutenant-ecosystem.github.io',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -73,7 +73,7 @@ const config: Config = {
       },
       items: [
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/lieutenant-ecosystem/lieutenant',
           label: 'GitHub',
           position: 'right',
         },
